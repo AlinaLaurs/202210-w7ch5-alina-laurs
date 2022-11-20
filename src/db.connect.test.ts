21f5a5b55c1c7d8e11e1231146ea7dfb1456e981
Alina <alilaursdesign@gmail.com>
2022-11-20 12:20:26 +0100

    Add testing

--- src/db.connect.test.ts
+++ src/db.connect.test.ts
@@ -9,6 +9,15 @@ describe('Given dbConnect', () => {
     });
 });
 
+describe('Given dbConnect', () => {
+    test('When we instantiate it should make a connection', async () => {
+        process.env.NODE_ENV = 'nottesting';
+        const result = await dbConnect();
+        expect(typeof result).toBe(typeof mongoose);
+        mongoose.disconnect();
+    });
+});
+
 describe('Given db.disconnect', () => {
     test('When we instantiate it should return 0', async () => {
         await dbConnect();
